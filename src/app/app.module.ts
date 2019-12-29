4c7952eb9b7436af917f48c20a302c2cf2af1e7d
sammyPro <samuel295@gmail.com>
2019-12-29 15:20:55 -0400

    Adding checkout function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CommentListComponent } from './comment-list/comment-list.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -17,6 +18,7 @@ import { CommentListComponent } from './comment-list/comment-list.component';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ])
   ],
   declarations: [
@@ -25,7 +27,8 @@ import { CommentListComponent } from './comment-list/comment-list.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CommentListComponent
+    CommentListComponent,
+    CartComponent
   ],
   bootstrap: [ AppComponent ]
 })
